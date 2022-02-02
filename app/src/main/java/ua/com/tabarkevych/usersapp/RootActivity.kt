57593cae8c9727f69de8cc50efa553df2e13e1d1
mixtab <m.tabarkevych@gmail.com>
2022-02-02 16:36:34 +0200

    Innit RootActivity

--- app/src/main/java/ua/com/tabarkevych/usersapp/RootActivity.kt
+++ app/src/main/java/ua/com/tabarkevych/usersapp/RootActivity.kt
@@ -2,10 +2,18 @@ package ua.com.tabarkevych.usersapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import ua.com.tabarkevych.usersapp.databinding.ActivityRootBinding
 
+@AndroidEntryPoint
 class RootActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRootBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_root)
+        setTheme(R.style.Theme_UsersApp)
+        binding = ActivityRootBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
