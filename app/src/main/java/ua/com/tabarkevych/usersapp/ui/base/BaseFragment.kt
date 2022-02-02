e1c6322bf20e495ccd2da13d448468a7eb86c7df
mixtab <m.tabarkevych@gmail.com>
2022-02-02 16:37:13 +0200

    Add BaseFragment

---
+++ app/src/main/java/ua/com/tabarkevych/usersapp/ui/base/BaseFragment.kt
@@ -0,0 +1,32 @@
+package ua.com.tabarkevych.usersapp.ui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<out T : ViewBinding> : Fragment() {
+
+    private var _binding: ViewBinding? = null
+
+    protected val binding: T
+        get() = _binding as T
+
+    protected abstract val bindingInflater: (LayoutInflater) -> ViewBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = bindingInflater(inflater)
+        return _binding!!.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
